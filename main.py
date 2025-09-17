27fc34c751270db50ef17b95d037bc62df1dc107
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2025-09-17 14:55:42 +0200

    add in main tkinter(which create window)

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-#Test
-print('Hello world')
+import tkinter
+import tkinter as tk
+
+
+
+if __name__ == '__main__':
+
+    root = tk.Tk()
+    root.title("Speed Math Calculator")
+    root.geometry("800x400")
+    root.mainloop()
+
