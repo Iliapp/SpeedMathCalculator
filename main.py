68057b167a98056e6df8136380d7f31541fd1894
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2025-09-19 11:29:17 +0200

    add levels, and button to chose these levels(Easy,Medium,Hard)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+import random
 
 
 if __name__ == '__main__':
@@ -13,9 +13,35 @@ if __name__ == '__main__':
     left_frame = tk.Frame(root,width=600,height=400,background="lightblue")
     left_frame.pack(side="left", fill="both", expand=True)
 
+    #Diffuculty levels
+    label = tk.Label(left_frame,text="Chose Difficulty and Start",font=("Arial", 16), bg="lightblue")
+    label.pack(pady=20)
+
+    options_list = ["Easy", "Medium", "Hard"]
+    value_inside = tk.StringVar(left_frame)
+    value_inside.set("")
+
+    question_menu = tk.OptionMenu(left_frame,value_inside, *options_list)
+    question_menu.pack(pady=10) # or grid
 
 
-    #Right Frame - Calculator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #Right FRAME - Calculator
     right_frame = tk.Frame(root,width=200,height=400,background="lightgrey")
     right_frame.pack(side="right", fill="y")
 
